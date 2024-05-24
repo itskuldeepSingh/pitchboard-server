fe11cc89859ca31e6eaa502f40126b25807cef8b
Kuldeep Singh <ks45991@gmail.com>
2024-05-24 16:44:46 +0530

    *

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://pitchboard.netlify.app/", // client code
+        origin: "https://pitchboard.netlify.app", // client code
         // methods: ["GET", "POST"],
     },
 });
